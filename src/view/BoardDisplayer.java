ab0c3948b8c44fa0327fb02ecb3cf819ce19d1f3
Lidor Rosencovich <lidor.rosencovich@sisense.com>
2018-12-22 16:19:34 +0200

    start of responsive canvas and connectin the server + images

--- src/view/BoardDisplayer.java
+++ src/view/BoardDisplayer.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -24,8 +25,70 @@ public class BoardDisplayer extends Canvas {
     private StringProperty dashPipeFileName;
     private StringProperty pipeLinePipeFileName;
     private StringProperty wallFileName;
+    private StringProperty startFileName;
+    private StringProperty goalFileName;
+
     private HashMap<String, Number>[][] coordinatesPixels;
 
+    @Override
+    public double minHeight(double width)
+    {
+        return 300;
+    }
+
+    @Override
+    public double maxHeight(double width)
+    {
+        return 10000;
+    }
+
+    @Override
+    public double prefHeight(double width)
+    {
+        return minHeight(width);
+    }
+
+    @Override
+    public double minWidth(double height)
+    {
+        return 0;
+    }
+
+    @Override
+    public double maxWidth(double height)
+    {
+        return 10000;
+    }
+
+    @Override
+    public boolean isResizable()
+    {
+        return true;
+    }
+
+    @Override
+    public void resize(double width, double height)
+    {
+        super.setWidth(width);
+        super.setHeight(height);
+        paint();
+    }
+
+    public String getStartFileName() {
+        return startFileName.get();
+    }
+
+    public void setStartFileName(String startFileName) {
+        this.startFileName.set(startFileName);
+    }
+
+    public String getGoalFileName() {
+        return goalFileName.get();
+    }
+
+    public void setGoalFileName(String goalFileName) {
+        this.goalFileName.set(goalFileName);
+    }
 
     public BoardDisplayer(){
         FPipeFileName = new SimpleStringProperty();
@@ -35,6 +98,8 @@ public class BoardDisplayer extends Canvas {
         dashPipeFileName = new SimpleStringProperty();
         pipeLinePipeFileName = new SimpleStringProperty();
         wallFileName = new SimpleStringProperty();
+        startFileName = new SimpleStringProperty();
+        goalFileName = new SimpleStringProperty();
     }
 
     private void InitCoordinatesPixels(int row, int col) {
@@ -110,7 +175,7 @@ public class BoardDisplayer extends Canvas {
         this.coordinatesPixels = coordinatesPixels;
     }
 
-    public void redraw(){
+    public void redraw() {
         if (board != null){
 
             String[] arrBoard = board.split("\n");
@@ -120,7 +185,7 @@ public class BoardDisplayer extends Canvas {
             double w = W / arrBoard[0].length();
             double h = H / arrBoard.length - 1; // without the 'done'
 
-            InitCoordinatesPixels(arrBoard[0].length(), arrBoard.length - 1);
+            //InitCoordinatesPixels(arrBoard[0].length(), arrBoard.length - 1);
 
             Image FImage = null;
             Image LImage = null;
@@ -129,6 +194,15 @@ public class BoardDisplayer extends Canvas {
             Image dashImage = null;
             Image pipeLineImage = null;
             Image wallImage = null;
+            Image startImage = null;
+            Image goalImage = null;
+
+            try {
+                String path = new File(".").getCanonicalPath();
+                System.out.println(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             try {
                 FImage = new Image(new FileInputStream(new File(getFPipeFileName())));
@@ -138,13 +212,15 @@ public class BoardDisplayer extends Canvas {
                 dashImage = new Image(new FileInputStream(new File(getDashPipeFileName())));
                 pipeLineImage = new Image(new FileInputStream(new File(getPipeLinePipeFileName())));
                 wallImage = new Image(new FileInputStream(new File(getWallFileName())));
+                startImage = new Image(new FileInputStream(new File(getStartFileName())));
+                goalImage = new Image(new FileInputStream(new File(getGoalFileName())));
             } catch (FileNotFoundException e){
                 e.printStackTrace();
             }
 
             GraphicsContext gc = getGraphicsContext2D();
 
-            gc.fillRect(0, 0, W, H);
+            gc.clearRect(0, 0, W, H);
 
             for (int i = 0 ; i < arrBoard.length - 1; i++){
                 for (int j = 0 ; j < arrBoard[i].length(); j++){
@@ -154,44 +230,63 @@ public class BoardDisplayer extends Canvas {
                             if (LImage != null){
                                 gc.drawImage(LImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                         case 'F': {
                             if (FImage != null){
                                 gc.drawImage(FImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                         case '7': {
                             if (sevenImage != null){
                                 gc.drawImage(sevenImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                         case 'J': {
                             if (JImage != null){
                                 gc.drawImage(JImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                         case '-': {
                             if (dashImage != null){
                                 gc.drawImage(dashImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                         case '|': {
                             if (pipeLineImage != null){
                                 gc.drawImage(pipeLineImage, j * w, i * h, w, h);
                             }
+                            break;
+                        }
+                        case 's': {
+                            if (startImage != null){
+                                gc.drawImage(startImage, j * w, i * h, w, h);
+                            }
+                            break;
+                        }
+                        case 'g': {
+                            if (goalImage != null){
+                                gc.drawImage(goalImage, j * w, i * h, w, h);
+                            }
+                            break;
                         }
                         case ' ': {
                             if (wallImage != null){
                                 gc.drawImage(wallImage, j * w, i * h, w, h);
                             }
+                            break;
                         }
                     }
 
                     // Set coordinates for the cell (later for mouse click indication)
-                    coordinatesPixels[i][j].put("startX", j*w);
+                    /*coordinatesPixels[i][j].put("startX", j*w);
                     coordinatesPixels[i][j].put("startY", i*h);
                     coordinatesPixels[i][j].put("endX", j*w + w);
-                    coordinatesPixels[i][j].put("endY", i*h + h);
+                    coordinatesPixels[i][j].put("endY", i*h + h);*/
                 }
             }
         }
